ac1432bcd7004a8206ffaf245de72caf0808ace7
IljaBlind <blindilya76@gmail.com>
2024-01-25 01:54:39 +0100

    hw5

---
+++ HomeWork/src/Hw5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Hw5 {
+    /**
+     * AIT-TR, Java Basic, Cohort 42-1, Hw 5
+     * @author Ilja Blind
+     * @version 24.01.2024
+     */
+    public static void main(String[] args) {
+
+        //task 1
+
+        //task 2
+
+        Scanner scanner = new Scanner(System.in);
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+    }
+}
