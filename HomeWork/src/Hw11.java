403b52a13696b585d0b04e76b09a4628917b3abe
IljaBlind <blindilya76@gmail.com>
2024-02-12 18:37:18 +0100

    n

---
+++ HomeWork/src/Hw11.java
@@ -0,0 +1,10 @@
+/**
+ * AIT-TR, Java Basic, Cohort 42-1, Hw 11
+ * @author Ilja Blind
+ * @version 12.01.2024
+ */
+public class Hw11 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ HomeWork/src/Hw12.java
@@ -0,0 +1,10 @@
+/**
+ * AIT-TR, Java Basic, Cohort 42-1, Hw 12
+ * @author Ilja Blind
+ * @version 13.01.2024
+ */
+public class Hw12 {
+    public static void main(String[] args) {
+
+    }
+}
